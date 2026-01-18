bc539d7f91847204926e6fd9401165e10726e642
Ashishhgurungg <ashishjeni123@gmail.com>
2026-01-18 22:48:24 +0545

    practice 3

---
+++ js_part1/practice3.js
@@ -0,0 +1,15 @@
+// Predict:
+console.log("10" + 1);  // 101
+console.log("10" - 1);  // 9
+console.log(true + false); // 1
+console.log(!!"Sheryians"); // True because !! changes the value into boolean
+
+//convert using unary
+let str = "42";
+let num = +str; // + converts into a number
+console.log(num);
+
+// ternary operator
+
+let age = 18;
+let answerr = age >= 18 ? "ELigible" : "Minor";
