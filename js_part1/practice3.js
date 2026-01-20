cfaf4f162b22ac0ee5042845c64da1c5297be03d
Ashishhgurungg <ashishjeni123@gmail.com>
2026-01-20 12:27:12 +0545

    pratice 3 done

--- js_part1/practice3.js
+++ js_part1/practice3.js
@@ -13,3 +13,39 @@ console.log(num);
 
 let age = 18;
 let answerr = age >= 18 ? "ELigible" : "Minor";
+
+//build a basic calculator
+
+function calc(a, b, operator) {
+    let calculation = {
+        "+": a + b,
+        "-": a - b,
+        "*": a * b,
+        "/": a / b,
+    }
+
+    return calculation[operator] ?? "Invalid operator or value";
+}
+
+// Print "Excellent", "Good", "Average", or "Fail" based on ranges
+
+
+function checkGrades(marks){
+    if (marks>=80 && marks<=100) {
+        console.log("excellent");
+        
+    } else if (marks>=60 && marks < 80) {
+        console.log("good");
+    } 
+    else if (marks>=40 && marks < 60) {
+        console.log("average");
+    }
+    
+    else if (marks < 40) {
+        console.log("Fail");
+    }
+    else
+    {
+        console.log("Invalid number");
+    }
+}
