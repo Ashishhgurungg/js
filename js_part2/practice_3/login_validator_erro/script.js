6de2ad3a941bd0c8e40923147c65080ed3e18277
Ashishhgurungg <ashishjeni123@gmail.com>
2026-02-06 15:25:39 +0545

    practice 3 done used uername instead of email validation

---
+++ js_part2/practice_3/login_validator_erro/script.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("#form");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+
+const userError = document.querySelector("#userError");
+const passwordError = document.querySelector("#passwordError");
+const success = document.querySelector("#success");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let isValid = true;
+    userError.style.display = "none";
+    passwordError.style.display = "none";
+    success.textContent = "";
+
+
+    const userRegex = /^[a-zA-Z][a-zA-Z0-9_]{2,19}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+
+    if (!userRegex.test(username.value)) {
+        userError.style.display = "block";
+        isValid = false;
+        
+    }
+    
+    if (!passwordRegex.test(password.value)) {
+        passwordError.style.display = "block";
+        isValid = false;
+    }
+
+    if (isValid) {
+        success.textContent = "All valid";
+    }
+})
