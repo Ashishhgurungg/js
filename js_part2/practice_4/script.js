615e961d09ee991485c71013077cdbbcf8900927
Ashishhgurungg <ashishjeni123@gmail.com>
2026-02-08 12:30:06 +0545

    practice 4 auto hide banner added

--- js_part2/practice_4/script.js
+++ js_part2/practice_4/script.js
@@ -1,34 +1,58 @@
 const button = document.querySelector(".buttons");
 const number = document.querySelector("#number");
+const alertBox = document.querySelector("#alert");
+
 let value = 0;
 let interUp = null;
 let interDown = null;
 
+function boxToggle() {
+    alertBox.classList.add("show");
+    setTimeout(() => {
+        alertBox.classList.remove("show");
+    }, 3000);
+}
+
 button.addEventListener("click", (e) => {
 
     if (interUp || interDown) return;
+
+    // COUNT UP
     if (e.target.matches("#up")) {
+        value = 0; // reset at start
+        number.textContent = value;
+
         interUp = setInterval(() => {
-            if (value > 10) {
+            number.textContent = value;
+
+            if (value === 10) {
                 clearInterval(interUp);
                 interUp = null;
-                value = 0;
+                boxToggle();
                 return;
             }
-            number.textContent = value++;
-        }, 1000);
 
-    }
-    if (e.target.matches("#down")) { 
-        value = 10;
-        interDown = setInterval(() => {
-            if (value < 0) {
-                clearInterval(interDown);
-                value = 0;
-                interDown = null;
-                return;
-            }
-            number.textContent = value--;
+            value++;
         }, 1000);
     }
-})
+
+    // COUNT DOWN
+    if (e.target.matches("#down")) {
+        value = 10; // reset at start
+        number.textContent = value;
+
+        interDown = setInterval(() => {
+            number.textContent = value;
+
+            if (value === 0) {
+                clearInterval(interDown);
+                interDown = null;
+                boxToggle()
+                return;
+            }
+
+            value--;
+        }, 1000);
+    }
+});
+
