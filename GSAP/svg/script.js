265c30a602b13f1551c65d2c0d08f8f857cfc020
Ashishhgurungg <ashishjeni123@gmail.com>
2026-03-14 15:07:43 +0545

    custom cursor

--- GSAP/svg/script.js
+++ GSAP/svg/script.js
@@ -1,10 +1,13 @@
-var path = `M 10 100 Q 500 100 1550 100`;
-var finalPath = `M 10 100 Q 500 100 1550 100`;
+var path = `M 10 200 Q 500 200 1550 200`;
+var finalPath = `M 10 200 Q 500 200 1550 200`;
+
+let cursor = document.querySelector("#cursor");
+let container = document.querySelector("#container");
 
 var string = document.querySelector("#string");
 // ${e.clientY}
 string.addEventListener("mousemove", (e) => {
-    path = `M 10 100 Q ${e.offsetX} ${e.offsetY} 1550 100`;
+    path = `M 10 200 Q ${e.offsetX} ${e.offsetY} 1550 200`;
 
     gsap.to("svg path", {
         attr: {
@@ -24,4 +27,21 @@ string.addEventListener("mouseleave", (e) => {
         duration: 1,
         ease: "elastic.out(1, 0.1)"
     })                               
+})
+
+window.addEventListener("mousemove", (e) => {
+
+
+    gsap.to(cursor, {
+        x: e.x,
+        y: e.y,
+
+    })
+})
+
+container.addEventListener( "mouseover", ()=> {
+ cursor.textContent = "String";
+ gsap.to(cursor, {
+    scale:2
+ })
 })
